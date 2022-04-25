56e016c97f2bbc046f854587e42e43f2f9ec421d
Safi Ullah Muhammad <safifiverr786@gmail.com>
2022-04-25 09:37:15 -0700

    Completed

--- app/src/main/java/com/example/newsapp/NewsAdapter.kt
+++ app/src/main/java/com/example/newsapp/NewsAdapter.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class NewsAdapter(val context: Context,val articles: List<Article>) :
     RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
@@ -25,6 +27,10 @@ class NewsAdapter(val context: Context,val articles: List<Article>) :
         val article = articles[position]
         holder.newsTitle.text = article.title
         holder.newsDescription.text = article.description
+        Glide.with(context).load(article.urlToImage).into(holder.newsImage)
+        holder.itemView.setOnClickListener {
+            Toast.makeText(context,article.title,Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int {
