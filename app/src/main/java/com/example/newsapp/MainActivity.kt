1b1bf12ef975d09abb273c02de0129b0f35f2b0f
Safi Ullah Muhammad <safifiverr786@gmail.com>
2022-04-25 08:46:21 -0700

    Title and Description

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.newsapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adapter : NewsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +24,10 @@ class MainActivity : AppCompatActivity() {
                 val news = response.body()
                 if (news != null){
                     Log.d("SAFI ULLAH", news.toString())
+                    adapter = NewsAdapter(this@MainActivity,news.articles)
+                    val newsList = findViewById<RecyclerView>(R.id.newsList)
+                    newsList.adapter = adapter
+                    newsList.layoutManager = LinearLayoutManager(this@MainActivity)
                 }
             }
 
