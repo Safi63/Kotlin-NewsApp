6d34d80622b1d36d19400a4ad913585c864ab9e9
Safi Ullah Muhammad <safifiverr786@gmail.com>
2022-04-25 05:08:36 -0700

    News fetched from API successfully

--- app/src/main/java/com/example/newsapp/News.kt
+++ app/src/main/java/com/example/newsapp/News.kt
@@ -1,3 +1,3 @@
 package com.example.newsapp
 
-data class News (val totalResult: Int,val article: List<Article>)
+data class News (val totalResults: Int,val articles: List<Article>)
--- app/src/main/java/com/example/newsapp/NewsService.kt
+++ app/src/main/java/com/example/newsapp/NewsService.kt
@@ -1,6 +1,9 @@
 package com.example.newsapp
 
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -14,5 +17,15 @@ interface NewsInterface{
 
     @GET("v2/top-headlines?apiKey=$API_KEY")
     fun getHeadLines(@Query("country")country:String,@Query("page")page:Int) : Call<News>
-    // https://newsapi.org/v2/top-headlines?apiKey=$API_KEY&country=us&page=1
+}
+
+object NewsService {
+    val newsInstance: NewsInterface
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        newsInstance = retrofit.create(NewsInterface::class.java)
+    }
 }
